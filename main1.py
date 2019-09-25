c571a12290a05f6f899c88df995605c98c780348
Beer <ambeer@iu.edu>
2019-09-25 16:59:38 -0400

    I changed the variable for GRAVITY

--- main1.py
+++ main1.py
@@ -14,7 +14,7 @@ MARGIN = 10
 NUM_BALLS = 20
 SCREEN_TITLE = "Gravity Exercise"
 
-GRAVITY = 0 #update this value
+GRAVITY = -9.8 #update this value
 
 
 class Ball():
@@ -65,6 +65,7 @@ class Window(arcade.Window):
     def update(self, delta_time):
         for b in self.ball_list:
             # apply gravity here
+            
             b.update()
 
     def on_draw(self):
